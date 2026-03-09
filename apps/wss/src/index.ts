db0ac82fd7993ce4c0529c907f73cdf17c032922
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2026-03-09 09:23:09 +0530

    refactor(repo): delete all projects from workspace

--- apps/wss/src/index.ts
+++
@@ -1,8 +0,0 @@
-import env from "#/configs/env.js";
-import server from "#/server.js";
-
-const port = env.PORT;
-
-server.listen(port, () => {
-  console.log(`Socket server listening on port: ${port}`);
-});
